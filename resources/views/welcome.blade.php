eee03f512a47e4168a25449dfdbaf7fcf4c752e8
Mochammad Dicky Hanun Prasetyo <dickyhanunprasetyo@gmail.com>
2024-08-28 21:08:58 +0700

    Upload Jobsheet 1 Tugas Membuat Biodata KTP

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>KTP Moch. Dicky Hanun P</title>
+    <title>KTP Mochammad Dicky Hanun P</title>
 </head>
 <body>
     <h1 align="center">PROVINSI JAWA TIMUR</h1>
