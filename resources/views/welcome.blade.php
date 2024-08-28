7b7649420482d1d387120078bd9f6aed24542a1e
Mochammad Dicky Hanun Prasetyo <dickyhanunprasetyo@gmail.com>
2024-08-28 21:02:38 +0700

    Upload Tugas Biodata KTP

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,74 @@
-<html lang="({ str_replace('_','-',app()->getlocale())})">
-    <head>
-        <meta charset ="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>PWL 2024</title>
-    </head>
-    <body>
-        <h2>Pemrograman Web Lanjut</h2>
-        <h3>.Semangat Belajar Laravel 10</h3>
-    </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>KTP Moch. Dicky Hanun P</title>
+</head>
+<body>
+    <h1 align="center">PROVINSI JAWA TIMUR</h1>
+    <h1 align="center">KABUPATEN MALANG</h1>
+    <hr width="600px">
+    <table align="center" cellpading="10px" width="600px">
+        <tr>
+            <td>NIK</td>
+            <td>: 350xxxxxxxxxxxx</td>
+            <td rowspan="11" align="center">
+                <img src="{{ asset('img/Dicky.jpg') }}" height="200px" width="170px" alt="Photo">
+                MALANG<br>
+                xx-xx-2021<br><br><br><br><br>
+                TTD
+            </td>
+        </tr>
+        <tr>
+            <td>Nama</td>
+            <td>: MOCHAMMAD DICKY HANUN PRASETYO</td>
+        </tr>
+        <tr>
+            <td>Tempat / Tgl Lahir</td>
+            <td>: MALANG, xx-xx-2004</td>
+        </tr>
+        <tr>
+            <td>Jenis Kelamin</td>
+            <td>: LAKI-LAKI &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Gol Darah : -</td>
+        </tr>
+        <tr>
+            <td>Alamat</td><br>
+            <td>: xxxxxxx</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>&nbsp;RT / RW &nbsp;&nbsp;&nbsp;&nbsp;: 01 / 01</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>&nbsp;Kel/Desa &nbsp;&nbsp;&nbsp;: xxxxxxx</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>&nbsp;Kecamatan : Bantur</td>
+        </tr>
+        <tr>
+            <td>Agama</td>
+            <td>: ISLAM</td>
+        </tr>
+        <tr>
+            <td>Status Perkawinan</td>
+            <td>: BELUM KAWIN</td>
+        </tr>
+        <tr>
+            <td>Pekerjaan</td>
+            <td>: PELAJAR/MAHASISWA</td>
+        </tr>
+        <tr>
+            <td>Kewarganegaraan</td>
+            <td>: WNI</td>
+        </tr>
+        <tr>
+            <td>Berlaku Hingga</td>
+            <td>: SEUMUR HIDUP</td>
+        </tr>
+    </table>
+</body>
 </html>
